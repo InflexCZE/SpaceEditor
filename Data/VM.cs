8e3ec1c200d780ab2076dcf3507bb850d1fb6c07
InflexCZE <martin.pavlicek11@gmail.com>
2025-07-26 17:44:37 +0200

    Bits for VM

--- Data/VM.cs
+++ Data/VM.cs
@@ -1,11 +1,34 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using SpaceEditor.Rocks;
 
 namespace SpaceEditor.Data;
 
 public abstract class VM : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
+    
+    public IDisposable Bind<T>(string property, Action<T> consumer)
+    {
+        var source = GetType().GetProperty(property)!.GetMethod;
+
+        consumer(Getvalue());
+        
+        PropertyChangedEventHandler handler = (sender, args) =>
+        {
+            consumer(Getvalue());
+        };
+        this.PropertyChanged += handler;
+        return Disposable.Create(() =>
+        {
+            this.PropertyChanged -= handler;
+        });
+        
+        T Getvalue()
+        {
+            return (T) source.Invoke(this, null);
+        }
+    }
     
     protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
     {
